77f39522596c68983763589c783f42b74d0e007f
omer-hayder <omer.goh@gmail.com>
2026-05-19 13:26:46 +0300

    Forms & Validations with signal

--- src/app/employee/employee-form/employee-form.ts
+++ src/app/employee/employee-form/employee-form.ts
@@ -1,38 +1,62 @@
-import { AsyncPipe, JsonPipe } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { JsonPipe, NgClass } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Employee } from '../../_services/employee';
+import { DepartmentService } from '../../_services/department-service';
+import { DepartmentDto } from '../../_models/department-dto';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [FormsModule, JsonPipe],
+  imports: [FormsModule, ReactiveFormsModule, NgClass],
   templateUrl: './employee-form.html',
   styleUrl: './employee-form.css',
 })
 export class EmployeeForm implements OnInit {
   private employeeService = inject(Employee);
+  private departmentService = inject(DepartmentService);
+  private fb = inject(FormBuilder);
+  private toastr = inject(ToastrService);
+
+  departmentList = signal<DepartmentDto[]>([]);
 
   constructor() {
     console.log("Hello from constructor");
   }
+
   ngOnInit(): void {
-    console.log("Hello from Init");
+    this.loadDepartments();
   }
 
-  employee: any = {
-    name: "",
-    departmentId: 0,
-    salary: 0,
-    projectIds: [1]
-  };
+
+  employeeForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    departmentId: [null, [Validators.required, Validators.min(1)]],
+    salary: [0, Validators.required],
+    projectIds: this.fb.array([])
+  });
+
+  get ef() { return this.employeeForm.controls }
+
+  loadDepartments() {
+    this.departmentService.getDepartments().subscribe({
+      next: (response) => { this.departmentList.set(response) },
+      error: (error) => { console.log(error); }
+    })
+  }
 
   save() {
-    console.log(this.employee);
+    console.log(this.employeeForm.value);
+    if (this.employeeForm.invalid) {
+      this.employeeForm.markAllAsTouched();
+      return;
+    }
 
-    this.employeeService.createEmployee(this.employee)
+    this.employeeService.createEmployee(this.employeeForm.value)
       .subscribe({
         next: (response) => {
-          console.log("Employee Created", response);
+          this.employeeForm.reset();
+          this.toastr.success("Employee Saved Successfully!", "Create Employee");
         },
         error: (error) => { console.log(error) }
       })
